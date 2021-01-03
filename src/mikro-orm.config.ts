a77d02099a2b862a23afa751498c04d05120c1da
Alan Laredo <AlanLaredo@hotmail.com>
2021-01-02 20:46:20 -0600

    create User entitie

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { Options } from "@mikro-orm/core";
 import * as path from "path";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 const options: Options = {
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "lilreddit",
   type: "postgresql",
   debug: !__prod__,
